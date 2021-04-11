1119da95ee72c4a9286af28349edddfe7560cabc
kartofka <44921893+kart0fka@users.noreply.github.com>
2021-04-11 17:37:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+window.onload = () => {
+    document.getElementById('loading').remove();
+}
+
+
+document.getElementById('menu-burger').onclick = () => {
+    var menu = document.getElementById('menu-burger');
+    if ( menu.getAttribute("class") == "toggled" ){
+        document.getElementById('menu').setAttribute("class", "");
+        menu.setAttribute("class", "");
+    }
+    else{
+        document.getElementById('menu').setAttribute("class", "opened");
+        menu.setAttribute("class", "toggled");
+    }
+}
+
+var menu_buttons = document.getElementsByClassName('menu-button');
+for ( var i = 0; i < menu_buttons.length; ++i ){
+    let button = menu_buttons[i];
+    button.onclick = (e) => {
+        e.preventDefault();
+        document.getElementById('menu-burger').onclick();
+        const id = button.hash;
+        let target = document.getElementById(id.slice(1));
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+
+    }
+}
