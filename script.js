d15324bbe2915efd69d1ac3e7611fd03e84af4ee
kartofka <44921893+kart0fka@users.noreply.github.com>
2021-04-18 14:45:32 +0300

    Update

--- script.js
+++ script.js
@@ -19,16 +19,18 @@ document.getElementById('menu-burger').onclick = () => {
 var menu_buttons = document.getElementsByClassName('menu-button');
 for ( var i = 0; i < menu_buttons.length; ++i ){
     let button = menu_buttons[i];
-    button.onclick = (e) => {
-        e.preventDefault();
-        document.getElementById('menu-burger').onclick();
-        const id = button.hash;
-        let target = document.getElementById(id.slice(1));
+    if ( button.hash != "" ){
+        button.onclick = (e) => {
+            e.preventDefault();
+            document.getElementById('menu-burger').onclick();
+            const id = button.hash;
+            let target = document.getElementById(id.slice(1));
 
-        document.querySelector(id).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
+            document.querySelector(id).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
 
+        }
     }
 }
